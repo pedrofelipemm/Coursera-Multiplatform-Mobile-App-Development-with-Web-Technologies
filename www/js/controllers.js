4ef75116ac98ed2568f4603209a70098bdef5c82
Pedro Felipe Marques Moreira <pedrofelipemm@gmail.com>
2016-04-21 21:14:13 -0300

    week1/Assignment

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -173,12 +173,12 @@ angular.module('conFusion.controllers', [])
           $scope.message = "Error: "+response.status + " " + response.statusText;
         }
       );
-    $scope.promotion = menuFactory.getPromotion().get({id:0});        
+    $scope.promotion = menuFactory.getPromotion().get({id:0});         
   }])
 
   .controller('AboutController', ['$scope', 'corporateFactory', function($scope, corporateFactory) {
-            
+    
+    $scope.ourHistory = 'Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.';
     $scope.leaders = corporateFactory.query();
-    console.log($scope.leaders);
             
   }]);
